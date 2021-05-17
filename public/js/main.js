0f64b49f62f184b89826d3f0a0f9d30e06603ac2
Abhinav <abhinav4999@gmail.com>
2021-05-17 18:19:13 +0530

    all UI except aysnc done

--- public/js/main.js
+++ public/js/main.js
@@ -24,3 +24,25 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
+
+
+const options = {
+  bottom: '32px', // default: '32px'
+  right: '32px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.3s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#00695c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: true, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
+window.addEventListener('load', addDarkmodeWidget);
+
+
